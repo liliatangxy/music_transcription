fb85103a65e9a16f9c0ad1dd0726e967acd17388
liliatangxy@gmail.com <liliatangxy@gmail.com>
2015-11-19 08:46:53 -0800

    modified output of pitch detection

--- src/note.py
+++ src/note.py
@@ -1,7 +1,6 @@
 import librosa
 
 class Note:
-
     def __init__(self, pitch, start, duration):
         self.pitch = pitch
         self.start = start
--- src/pitch.py
+++ src/pitch.py
@@ -1,6 +1,7 @@
 import librosa
+import numpy as np
 
-from note import Note
+import note
 
 '''
 Functions to process pitches
@@ -20,25 +21,28 @@ def determine_pitches(y, sr):
     Determines pitches
     '''
     y_harmonic = librosa.effects.harmonic(y)
-    chromagram = librosa.feature.chroma_stft(y_harmonic, sr)
-    # TODO store notes in order they appear, not order they end
+    d = librosa.logamplitude(librosa.stft(y_harmonic, sr)**2, ref_power=np.max)
+    
     note_list = [] # stores note with pitch, start time, duration in time
-    current_d = [0] * 12 # current duration of note in loop
-    threshold = 0.9
-    for i in range(0, chromagram[0].size):
-        for j in range(0, 12):
-            if chromagram[j][i] >= threshold:
-                current_d[j] += 1
-                if i == chromagram[0].size - 1:
-                    start = librosa.core.frames_to_time(i)
-                    duration = librosa.core.frames_to_time(current_d[j])
-                    new_note = Note(j, start, duration)
+
+    current_n = [[0,0]] * d.shape[0] # current [start,duration] of note in loop
+    threshold = -8
+    for i in range(0, d.shape[1]):
+        for j in range(0, d.shape[0]):
+            if d[j][i] >= threshold:
+                current_n[j][1] += 1
+                if i == d.shape[0] - 1:
+                    start = librosa.core.frames_to_time(current_n[j][0])
+                    duration = librosa.core.frames_to_time(current_n[j][1])
+                    new_note = note.Note(j, start, duration)
                     note_list.append(new_note)
-            elif current_d[j] != 0:
-                # TODO store pitch with octave
-                start = librosa.core.frames_to_time(i)
-                duration = librosa.core.frames_to_time(current_d[j])
+            elif current_n[j][1] != 0:
+                start = librosa.core.frames_to_time(current_n[j][0])
+                duration = librosa.core.frames_to_time(current_n[j][1])
                 new_note = Note(j, start, duration)
                 note_list.append(new_note)
-                current_d[j] = 0
+                current_n[j][0] = 0
+                current_n[j][1] = 0
+    # TODO may need to implement more efficient sort since notes are likely only unsorted within one measure
+    note_list.sort(key=lambda n: n.start)
     return note_list
